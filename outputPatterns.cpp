14972981adfb448b5750d41f1f67f9ff94c55282
Joseph McGowan <josephdmcgowan68@gmail.com>
2024-05-09 16:19:33 +0100

    OutputPatterns

---
+++ outputPatterns.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void printRightTriangle(int n) {
+    for (int i = n; i > 0; i--) {
+        for (int j = i; j > 0; j--)
+            cout << '#';
+        cout << "\n";
+    }
+}
+
+void printSidewaysTriangle (int n = 5) {
+    //for loop that peaks in middle
+    int i = n * 2;
+
+    for (int j = 1; j < i; j++) {
+        for (int y = 0; y < n - abs(n - j); y++)
+            cout << '#';
+        cout << '\n';
+    }
+
+
+    // printRightTriangle(n);
+
+
+
+
+
+}
+
+
+
+int main() {
+
+    printSidewaysTriangle();
+    return 0;
+}
