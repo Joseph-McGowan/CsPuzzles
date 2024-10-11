da25089831db2ccbcfe0bedc0c6fc6088f5f3bf3
Joseph McGowan <josephdmcgowan68@gmail.com>
2024-10-11 10:47:10 +0100

    6.1 boilerplate

---
+++ Recursion/6.1.cpp
@@ -0,0 +1,20 @@
+//
+// Created by joseph on 11/10/2024.
+//
+#include "iostream"
+
+using namespace std;
+
+int sumPositiveNumbs(int vals[], int size) {
+    //find sum of all positive numbers in array
+    //base case, array size = 0;
+    //recursive case, if last array value is postive, add to sumPosNumbs(size - 1)
+    //else, add 0 to sumPosNumbs(size -1)
+
+
+}
+
+
+int main() {
+
+}
