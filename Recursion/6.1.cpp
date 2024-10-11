5a167fa58442fe44f4f6b6c4f25352693f2c2e0f
Joseph McGowan <josephdmcgowan68@gmail.com>
2024-10-11 20:47:56 +0100

    6.1 complete

--- Recursion/6.1.cpp
+++ Recursion/6.1.cpp
@@ -11,10 +11,20 @@ int sumPositiveNumbs(int vals[], int size) {
     //recursive case, if last array value is postive, add to sumPosNumbs(size - 1)
     //else, add 0 to sumPosNumbs(size -1)
 
+    if (size == 0)
+        return 0;
+    else {
+        if (vals[size - 1] > 0)
+            return vals[size-1] + sumPositiveNumbs(vals, size - 1);
+        else
+            return 0 + sumPositiveNumbs(vals, size - 1);
+    }
 
 }
 
 
 int main() {
+    int test[] = {3,-1,5,7,-5,0,5,1};
 
+    cout << sumPositiveNumbs(test, 8);
 }
